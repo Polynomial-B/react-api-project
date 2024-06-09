c3893d3bd2190b92b88d841a1182968741245c07
Matt <mphlamb@gmail.com>
2024-06-09 21:10:02 +0200

    initial commit

--- src/components/MiddleEarth.jsx
+++ src/components/MiddleEarth.jsx
@@ -1,32 +1,41 @@
 import React from "react";
 import "../App.css";
+import "./MiddleEarth.css";
+
 import lordOfTheRings from "../assets/bookId";
 
+const { VITE_AUTH_TOKEN } = import.meta.env
+
 function MiddleEarth() {
+  const [chapters, setChapters] = React.useState([]);
   const [hasRead, setHasRead] = React.useState([]);
-  const [chapter, setChapter] = React.useState([]);
-
-
 
     React.useEffect(() => {
-      async function fetchChapter() {
+      async function fetchChapters() {
         const response = await fetch(`https://the-one-api.dev/v2/chapter`, {
-          headers: { Authorization: "Bearer cnvIvbglprwk70DAWtdb" },
+          headers: { Authorization: `Bearer ${VITE_AUTH_TOKEN}` },
         });
-        const chapter = await response.json();
-        setChapter(chapter.docs);
+        const chaptersResponse = await response.json();
+        const { docs: chapters } = chaptersResponse
+        setChapters(chapters);
       }
 
-      fetchChapter();
+      fetchChapters();
     }, []);
 
-function handleCompletedChapter(e) {
-  console.log(e.target)
-  const newChapterRead = structuredClone(hasRead)
-  newChapterRead.push(e)
-  setHasRead(newChapterRead)
-  // console.log(hasRead)
+function handleCompletedChapter(clickedChapter) {
+  if (hasRead.includes(clickedChapter)) {
+    return
+  }
+  console.log('Selected chapter', clickedChapter)
+  const hasReadClone = [...hasRead] // 1. structured clone creates new objects different from 'chapters' array above (unlinked)
+  hasReadClone.push(clickedChapter) // 2. add clicked chapter with same obj reference as 'chapters' above (linked)
+  setHasRead(hasReadClone) // 3. setting state with new references to read chapters apart from latest clicked one (2.)
+  console.log('read', hasReadClone)
 
+  // const readIdsClone = [...readIds]
+  // readIdsClone.push(clickedChapter._id);
+  // setReadIds(readIdsClone)
 }
 
 
@@ -34,13 +43,14 @@ function handleCompletedChapter(e) {
     return (
       <>
         <h1>Book Tracker</h1>
-        <div className="container"
-        onClick={(e) => handleCompletedChapter(e)}
-        >
-          {chapter.map(ch => {
+
+        <div>{hasRead.map(chapter => chapter.chapterName).join(', ')}</div>
+        <div className="container">
+          {chapters.map((chapter, idx) => {
             return (
-              <div className="chapter-div" id={ch.book.includes("1582") ? "fellowship" : ch.book.includes("1583") ? "two-towers" : ch.book.includes("1584") ? "return-of-the-king" : ""} key={ch._id}>
-                {ch.chapterName}
+              <div className={`chapter-div${hasRead.includes(chapter) ? ' read' : ''}`} id={chapter.book.includes("1582") ? "fellowship" : chapter.book.includes("1583") ? "two-towers" : chapter.book.includes("1584") ? "return-of-the-king" : ""} key={idx}
+              onClick={() => handleCompletedChapter(chapter)}>
+                {chapter.chapterName}
                 <img></img>
               </div>
             );
